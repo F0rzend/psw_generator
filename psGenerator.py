9e3398477963f8f1375295f964189d9f7a37a7a6
Kyle <kostyajog@mail.ru>
2019-10-11 21:02:53 +0300

    Добавленны циклы на случай неверного ввода

--- psGenerator.py
+++ psGenerator.py
@@ -1,23 +1,38 @@
 import random
 import string
 
-
 pass_list = tuple()
-choice = input('Использовать ли маленькие буквы в пароле? (y/n): ')
-if choice == 'y':
-    pass_list += tuple(string.ascii_lowercase)
-choice = input('Использовать ли большие буквы в пароле? (y/n): ')
-if choice == 'y':
-    pass_list += tuple(string.ascii_uppercase)
-choice = input('Использовать ли цифры в пароле? (y/n): ')
-if choice == 'y':
-    pass_list += tuple(string.digits)
-choice = input('Использовать ли символы? (y/n): ')
-if choice == 'y':
-    pass_list += tuple(string.punctuation)
 
-length = input('Какова длинна пароля?: ')
-count = input('Сколько паролей создать вам для выбора?: ')
+choice = None
+while choice != 'y' and choice != 'n':
+    choice = input('Использовать ли маленькие буквы в пароле? (y/n): ')
+    if choice == 'y':
+        pass_list += tuple(string.ascii_lowercase)
+
+choice = input('Использовать ли большие буквы в пароле? (y/n): ')
+while choice != 'y' and choice != 'n':
+    if choice == 'y':
+        pass_list += tuple(string.ascii_uppercase)
+
+choice = input('Использовать ли цифры в пароле? (y/n): ')
+while choice != 'y' and choice != 'n':
+    if choice == 'y':
+        pass_list += tuple(string.digits)
+
+choice = input('Использовать ли символы? (y/n): ')
+while choice != 'y' and choice != 'n':
+    if choice == 'y':
+        pass_list += tuple(string.punctuation)
+
+length = ''
+while not length.isdigit():
+    length = input('Какова длинна пароля?: ')
+
+count = ''
+while not count.isdigit():
+    count = input('Сколько паролей создать вам для выбора?: ')
+
+
 password = ''
 result = {}
 
